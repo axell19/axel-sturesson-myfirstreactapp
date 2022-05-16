0e3bf21ce2e57e543aeaabc456736040dbf743b1
axell19 <70134982+axell19@users.noreply.github.com>
2022-05-16 08:41:23 +0200

    react

---
+++ src/navigation/navigation.component.jsx
@@ -0,0 +1,11 @@
+import './navigation.styles.css'
+
+function navigation (){
+    return (
+        <div>
+            <h1 className="nav">Im the navigation</h1>
+        </div>
+    )
+}
+
+export default navigation; 
